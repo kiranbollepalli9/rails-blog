2dcf3a0b98abd8cd2a9094b1ca882e5a6717454b
kiranbollepalli9 <kiranbollepalli@gmail.com>
2020-05-15 14:59:24 +0530

    Added users table with validations

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+
+    validates :username, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: {in: 3..20}
+    
+    EMAIL_VALIDATION_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i;
+
+    validates :email, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: {in: 6..50},
+                        format: {with: EMAIL_VALIDATION_REGEX }                    
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_123003) do
+ActiveRecord::Schema.define(version: 2020_05_15_090855) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
